8881028d6b8998ddfbc8e6a492205d269e2e5ddd
CarlosBaessa <carlosbaessa34@gmail.com>
2024-01-22 12:59:01 -0100

    responsividd foot

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -10,9 +10,9 @@ const Footer = () => {
         data-wow-delay=".1s"
       >
         <div className="container">
-          <div className="-mx-4 flex flex-wrap">
+          <div className="-mx-4 flex flex-wrap text-center lg:text-left">
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
-              <div className="mb-4 max-w-[360px] lg:mb-14">
+              <div className="mb-4 lg:mb-14">
                 <Link href="/" className="mb-2 inline-block">
                   <Image
                     src="/images/logo/logos_lettering_blue-24.png"
@@ -70,7 +70,7 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-3/12">
+            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-4/12 xl:w-3/12">
               <div className="mb-12 lg:mb-16">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
                   Política de Privacidade
@@ -109,7 +109,7 @@ const Footer = () => {
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
                   Redes Sociais 
                 </h2>
-                <div className="flex items-center">
+                <div className="flex items-center justify-center">
                   <a
                     href=""
                     aria-label="social-link"
@@ -175,7 +175,7 @@ const Footer = () => {
           <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
           <div className="py-8">
             <p className="text-center text-base text-body-color dark:text-white">
-            Copyright ©2024 CV Innovation All Right Reserve
+            Copyright ©2024 CV Innovation All Right Reserved
             </p>
           </div>
         </div>
